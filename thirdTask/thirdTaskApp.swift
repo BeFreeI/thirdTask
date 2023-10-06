d26d570f2dbd23bcf13537dc8791ce33beced86c
TURKCELL/Pavel Nepogodin <pavel.nepogodin@consultant.turcell.com.tr>
2023-10-06 22:08:47 +0300

    Initial Commit

---
+++ thirdTask/thirdTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  thirdTaskApp.swift
+//  thirdTask
+//
+//  Created by  Pavel Nepogodin on 6.10.23.
+//
+
+import SwiftUI
+
+@main
+struct thirdTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
